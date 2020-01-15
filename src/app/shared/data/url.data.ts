6e8e2cd0421ef874fd1b3496659b35f87134afb8
Leonid <leonid.vakar@gmail.com>
2020-01-15 20:57:25 +0200

    chore: switch to new API address

--- src/app/shared/data/url.data.ts
+++ src/app/shared/data/url.data.ts
@@ -1,6 +1,6 @@
 export class UrlHolder {
-  static WEATHER_API = "http://rest.vakar.space/weather/weather/";
-  static CITY_API = "http://rest.vakar.space/weather/city/";
+  static WEATHER_API = "http://stuff.vakar.space/weather/weather/";
+  static CITY_API = "http://stuff.vakar.space/weather/city/";
 
   static getWeatherIconUrl(iconName: string) {
     return "http://openweathermap.org/img/wn/" + iconName + "@2x.png";
