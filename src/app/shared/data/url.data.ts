36c0958487ae2e018c7fc0cda6770e1e79f75524
Leonid <leonid.vakar@gmail.com>
2020-06-13 15:21:03 +0300

    chore (app): change data server

--- src/app/shared/data/url.data.ts
+++ src/app/shared/data/url.data.ts
@@ -1,6 +1,6 @@
 export class UrlHolder {
-  static WEATHER_API = "http://stuff.vakar.space/weather/weather/";
-  static CITY_API = "http://stuff.vakar.space/weather/city/";
+  static WEATHER_API = "http://3.22.165.240:8080/weather/";
+  static CITY_API = "http://3.22.165.240:8080/city/";
 
   static getWeatherIconUrl(iconName: string) {
     return "http://openweathermap.org/img/wn/" + iconName + "@2x.png";
